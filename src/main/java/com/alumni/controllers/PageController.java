e5ce43d5dbf528d62655fde6f190bfa9e1988ced
Kshitiz Gupta <kshitiz@gmail.com>
2024-06-14 19:25:17 +0530

    Updating User Service

--- src/main/java/com/alumni/controllers/PageController.java
+++ src/main/java/com/alumni/controllers/PageController.java
@@ -7,11 +7,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// import com.scm.entities.User;
-// import com.scm.forms.UserForm;
+import com.alumni.forms.UserForm;
+
+import com.alumni.entities.User;
+ import com.alumni.forms.UserForm;
 // import com.scm.helpers.Message;
 // import com.scm.helpers.MessageType;
-// import com.scm.services.UserService;
+import com.alumni.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -21,6 +23,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
 
     @RequestMapping("/home")
     public String home(){
@@ -54,9 +64,74 @@ public class PageController {
         return "login";
     }
 
+
+    // registration page
     @GetMapping("/register")
-    public String register() {
-        System.out.println("register page loading");
+    public String register(Model model) {
+
+        UserForm userForm = new UserForm();
+        // default data bhi daal sakte hai
+        // userForm.setName("Kshitiz");
+        // userForm.setAbout("This is about : Write something about yourself");
+        model.addAttribute("userForm", userForm);
+
         return "register";
+    }
+
+    // processing register
+
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
+            HttpSession session) {
+        System.out.println("Processing registration");
+        // fetch form data
+        // UserForm
+        System.out.println(userForm);
+
+        // validate form data
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
+
+        // TODO::Validate userForm[Next Video]
+
+        // save to database
+
+        // userservice
+
+        // UserForm--> User
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .password(userForm.getPassword())
+        // .about(userForm.getAbout())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .profilePic(
+        // "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75")
+        // .build();
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        User savedUser = userService.saveUser(user);
+
+        System.out.println("user saved :");
+
+        // message = "Registration Successful"
+
+        // add the message:
+
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
+        session.setAttribute("message", message);
+
+        // redirectto login page
+        return "redirect:/register";
     }
 }
---
+++ src/main/java/com/alumni/helpers/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.alumni.helpers;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
+}
